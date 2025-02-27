e7c78b01cc3690094dfefab8d40a6a0018928ea8
Asma Ghafoor <asmaghafoorofficial@gmail.com>
2025-02-27 15:57:47 +0500

    Node JS with MongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,24 +1,11 @@
-// const { MongoClient } = require('mongodb');
-// const url = 'mongodb://localhost:27017'
-// const databaseName = 'e-commerce'
-// const client = new MongoClient(url);
-
-// async function dbConnect() {
-//     let result = await client.connect();
-//     db = result.db(databaseName);
-//     return db.collection('products');
-// }
-
-// module.exports = dbConnect;
-
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const databaseName = 'e-commerce';
+const url = 'mongodb://localhost:27017'
+const databaseName = 'e-commerce'
 const client = new MongoClient(url);
 
 async function dbConnect() {
     let result = await client.connect();
-    let db = result.db(databaseName);
+    db = result.db(databaseName);
     return db.collection('products');
 }
 
